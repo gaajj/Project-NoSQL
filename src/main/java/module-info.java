c9dac8840c8d91639861472713b4a5f8b5bc7b0a
gaxzy <daantas224@gmail.com>
2025-01-01 19:55:46 +0100

    Init2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module example.projectsigma6 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.mongodb.bson;
+    requires java.xml;
+    requires org.mongodb.driver.sync.client;
+    requires org.mongodb.driver.core;
 
 
-    opens example.projectsigma6 to javafx.fxml;
-    exports example.projectsigma6;
+    opens org.example.projectsigma6 to javafx.fxml;
+    exports org.example.projectsigma6;
+    exports org.example.projectsigma6.tests;
 }
---
+++ src/main/java/org/example/projectsigma6/MainApp.java
@@ -0,0 +1,4 @@
+package org.example.projectsigma6;
+
+public class MainApp {
+}
