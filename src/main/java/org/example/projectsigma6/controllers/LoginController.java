4851560ba51703deed4f4b3a7e2b454d2af701f7
gaxzy <daantas224@gmail.com>
2025-01-11 19:38:34 +0100

    Add login handler

--- src/main/java/org/example/projectsigma6/controllers/LoginController.java
+++ src/main/java/org/example/projectsigma6/controllers/LoginController.java
@@ -1,6 +1,9 @@
 package org.example.projectsigma6.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import org.example.projectsigma6.MainApp;
+import org.example.projectsigma6.models.Employee;
 import org.example.projectsigma6.services.EmployeeService;
 import org.example.projectsigma6.services.ServiceManager;
 
@@ -9,13 +12,26 @@ public class LoginController {
     private MainApp mainApp;
     private EmployeeService employeeService;
 
+    @FXML
+    private TextField usernameField;
+
     public LoginController(MainApp mainApp) {
         this.mainApp = mainApp;
         this.employeeService = ServiceManager.getInstance().getEmployeeService();
     }
 
-    public void initialize() {
+    @FXML
+    public void handleLogin() {
+        String username = usernameField.getText();
 
+        Employee loggedInEmployee = employeeService.getEmployeeByUsername(username);
+
+        if (loggedInEmployee != null) {
+            mainApp.setLoggedInEmployee(loggedInEmployee);
+            System.out.println("[+] Logged in as: " + loggedInEmployee.getUsername());
+        } else {
+            System.out.println("[-] Invalid username.");
+        }
     }
 
 }
