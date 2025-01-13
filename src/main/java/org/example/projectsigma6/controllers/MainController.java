06712434ff04fc4da0447bcad66bf41fe9dd1e72
gaxzy <daantas224@gmail.com>
2025-01-13 13:57:40 +0100

    Add MainController

---
+++ src/main/java/org/example/projectsigma6/controllers/MainController.java
@@ -0,0 +1,27 @@
+package org.example.projectsigma6.controllers;
+
+import javafx.fxml.FXML;
+import org.example.projectsigma6.MainApp;
+
+public class MainController {
+
+    private MainApp mainApp;
+
+    public MainController(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+
+    @FXML
+    public void handleLogout() {
+        System.out.println("[*] Logged out");
+        mainApp.setLoggedInEmployee(null);
+        mainApp.loadPage("Login.fxml", new LoginController(mainApp), null);
+    }
+
+    @FXML
+    public void showDashboard() {
+        System.out.println("[+] Dashboard");
+        mainApp.show("DashboardView.fxml", new DashboardController(mainApp));
+    }
+
+}
