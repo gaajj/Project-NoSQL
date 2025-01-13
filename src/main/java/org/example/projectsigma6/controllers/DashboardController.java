3eefe3a5db2b401cbf07195bd7d5ad77912c9e16
gaxzy <daantas224@gmail.com>
2025-01-13 13:10:10 +0100

    Add PieChart

--- src/main/java/org/example/projectsigma6/controllers/DashboardController.java
+++ src/main/java/org/example/projectsigma6/controllers/DashboardController.java
@@ -1,21 +1,30 @@
 package org.example.projectsigma6.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
 import org.example.projectsigma6.MainApp;
 import org.example.projectsigma6.models.Employee;
+import org.example.projectsigma6.models.Ticket;
+import org.example.projectsigma6.models.enums.TicketStatus;
+import org.example.projectsigma6.services.ServiceManager;
+
+import java.util.List;
 
 public class DashboardController {
 
     private MainApp mainApp;
     private Employee loggedInEmployee;
 
+    private int openTickets;
+    private int closedTickets;
+    private int inProgressTickets;
+
     @FXML
-    private Label loggedInUsernameLabel;
-    @FXML
-    private Label loggedInFullNameLabel;
-    @FXML
-    private Label loggedInEmailLabel;
+    private PieChart ticketStatsPieChart;
 
     public DashboardController(MainApp mainApp) {
         this.mainApp = mainApp;
@@ -24,12 +33,30 @@ public class DashboardController {
 
     @FXML
     public void initialize() {
+        updateTicketStats();
     }
 
-    public void displayLoggedInUserInfo() {
-        loggedInUsernameLabel.setText(loggedInEmployee.getUsername());
-        loggedInFullNameLabel.setText(loggedInEmployee.getFirstName() + " " + loggedInEmployee.getLastName());
-        loggedInEmailLabel.setText(loggedInEmployee.getEmail());
+    public void updateTicketStats() {
+        getTicketData();
+
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+                new PieChart.Data("Open", openTickets),
+                new PieChart.Data("Closed", closedTickets),
+                new PieChart.Data("In Progress", inProgressTickets)
+        );
+        ticketStatsPieChart.setData(pieChartData);
+    }
+
+    public void getTicketData() {
+        List<Ticket> Tickets = ServiceManager.getInstance().getTicketService().getAllTickets();
+
+        for (Ticket ticket : Tickets) {
+            switch (ticket.getStatus()) {
+                case TicketStatus.OPEN -> openTickets++;
+                case TicketStatus.CLOSED -> closedTickets++;
+                case TicketStatus.IN_PROGRESS -> inProgressTickets++;
+            }
+        }
     }
 
 }
